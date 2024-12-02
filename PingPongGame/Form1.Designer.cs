00180155062678f1ee5554e417825db433a63f3e
Udyan Trivedi <43742293+udyancoder@users.noreply.github.com>
2024-12-02 20:01:30 +0530

    Add project files.

---
+++ PingPongGame/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PingPongGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.Computer = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Computer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.Image = global::PingPongGame.Properties.Resources.ball;
+            this.ball.Location = new System.Drawing.Point(359, 220);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // Computer
+            // 
+            this.Computer.Image = global::PingPongGame.Properties.Resources.computer;
+            this.Computer.Location = new System.Drawing.Point(1032, 270);
+            this.Computer.Name = "Computer";
+            this.Computer.Size = new System.Drawing.Size(30, 120);
+            this.Computer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Computer.TabIndex = 1;
+            this.Computer.TabStop = false;
+            this.Computer.Click += new System.EventHandler(this.Computer_Click);
+            // 
+            // Player
+            // 
+            this.Player.Image = global::PingPongGame.Properties.Resources.player;
+            this.Player.Location = new System.Drawing.Point(1, 197);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(30, 120);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            this.Player.Click += new System.EventHandler(this.Player_Click);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuText;
+            this.ClientSize = new System.Drawing.Size(1060, 671);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.Computer);
+            this.Controls.Add(this.Player);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Player: 0 -- Computer: 0";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Computer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox Computer;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ PingPongGame/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPongGame
+{
+    public partial class Form1 : Form
+    {
+        private int originalWidth;
+        private int originalHeight;
+        int ballXspeed = 4;
+        int ballYspeed = 4;
+        int speed = 2;
+        Random rand = new Random();
+        bool goDown, goUp;
+        int computer_speed_change = 50;
+        int PlayerScore = 0;
+        int ComputerScore = 0;
+        int PlayerSpeed = 8;
+        private const int DefaultPlayerSpeed = 8;
+        int[] i = { 10, 11, 16, 18};
+        int[] j = { 10, 11, 16, 18};
+        private int difficultyLevel = 1;
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            float widthScale = (float)this.ClientSize.Width / originalWidth;
+            float heightScale = (float)this.ClientSize.Height / originalHeight;
+
+            // Adjust paddle sizes
+            Player.Width = (int)(20 * widthScale);
+            Player.Height = (int)(100 * heightScale);
+
+            Computer.Width = (int)(20 * widthScale);
+            Computer.Height = (int)(100 * heightScale);
+
+            // Adjust ball size
+            ball.Width = (int)(20 * widthScale);
+            ball.Height = (int)(20 * heightScale);
+
+            // Adjust positions proportionally
+            Player.Left = 20; // Fixed position on the left
+            Player.Top = (int)(Player.Top * heightScale); // Scale its position
+
+            Computer.Left = this.ClientSize.Width - Computer.Width - 20; // Align to the right edge
+            Computer.Top = (int)(Computer.Top * heightScale); // Scale its position
+
+            ball.Left = (int)(ball.Left * widthScale);
+            ball.Top = (int)(ball.Top * heightScale);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            originalWidth = this.ClientSize.Width;
+            originalHeight = this.ClientSize.Height;
+            this.Resize += new EventHandler(Form1_Resize);
+        }
+
+        private void Player_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            ball.Top -= ballYspeed;
+            ball.Left -= ballXspeed;
+
+            this.Text = "Player Score: " + PlayerScore + " -- Computer Score: " +
+                ComputerScore;
+            if(ball.Top < 0 || ball.Bottom > this.ClientSize.Height)
+            {
+                ballYspeed = -ballYspeed;
+            }
+            if(ball.Left < -2) // computer score
+            {
+                ball.Left = 300;
+                ballXspeed = -ballXspeed;
+                ComputerScore++;
+            }
+
+            if (ball.Right > this.ClientSize.Width + 2)
+            {
+                ball.Left = 300;
+                ballXspeed = -ballXspeed;
+                PlayerScore++;
+            }
+            if(Computer.Top <= 1)
+            {
+                Computer.Top = 0;
+            }
+            else if (Computer.Bottom >= this.ClientSize.Height)
+            {
+                Computer.Top = this.ClientSize.Height - Computer.Height;
+            } 
+
+            if(ball.Top < Computer.Top + (Computer.Height/2)&& ball.Left > 300)
+            {
+                Computer.Top -= speed;
+            }
+            if (ball.Top > Computer.Top + (Computer.Height / 2) && ball.Left > 300)
+            {
+                Computer.Top += speed;
+            }
+
+            computer_speed_change -= 1;
+            if(computer_speed_change < 0)
+            {
+                speed = i[rand.Next(i.Length)];
+                computer_speed_change = 50;
+            }
+
+            if(goDown && Player.Top + Player.Height < this.ClientSize.Height)
+            {
+                Player.Top += PlayerSpeed;
+            }
+
+            if(goUp && Player.Top > 0)
+            {
+                Player.Top -= PlayerSpeed;
+            }
+
+            CheckCollision(ball, Player, Player.Right + 5);
+            CheckCollision(ball, Computer, Computer.Left - 35);
+
+            if(ComputerScore > 3)
+            {
+                GameOver("Sorry, You lost the Game. Better Luck Next Time");
+            }
+            else if(PlayerScore > 3)
+            {
+                GameOver("Congratulations! You did it");
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.S)
+            {
+                goDown = true;
+            }
+            if(e.KeyCode == Keys.W)
+            {
+                goUp = true;
+            }
+               
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.S)
+            {
+                goDown = false;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                goUp = false;
+            }
+        }
+
+        private void CheckCollision(PictureBox PicOne, PictureBox PicTwo, int offset)
+        {
+            if (PicOne.Bounds.IntersectsWith(PicTwo.Bounds))
+            {
+                PicOne.Left = offset;
+
+                int x = j[rand.Next(j.Length)];
+                int y = j[rand.Next(j.Length)];
+
+                if(ballXspeed < 0)
+                {
+                    ballXspeed = x;
+                }
+                else
+                {
+                    ballXspeed = -x;
+                }
+
+                if (ballYspeed < 0)
+                {
+                    ballYspeed = -y;
+                }
+                else
+                {
+                    ballYspeed = y;
+                }
+
+
+            }
+        }
+
+        private void Computer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameOver(string message)
+        {
+            GameTimer.Stop();
+            MessageBox.Show(message, "Udyan Says: ");
+
+            // Reset game scores
+            ComputerScore = 0;
+            PlayerScore = 0;
+
+            // Reset ball speed
+            ballXspeed = ballYspeed = 4;
+
+            if (message.Contains("Congratulations")) // Player wins
+            {
+                // Increase difficulty
+                difficultyLevel++;
+                speed += 1; // Increase computer speed
+                computer_speed_change = Math.Max(10, computer_speed_change - 5); // Reduce reaction time, minimum 10
+                PlayerSpeed = DefaultPlayerSpeed; // Reset player speed on win
+            }
+            else // Player loses
+            {
+                difficultyLevel = 1; // Reset difficulty level
+                speed = 2;           // Reset computer speed
+                computer_speed_change = 50; // Reset reaction time
+
+                // Increase player speed to make it easier in the next round
+                PlayerSpeed = Math.Min(20, PlayerSpeed + 2); // Cap player speed at 20
+            }
+
+            // Restart the game
+            GameTimer.Start();
+        }
+    }
+}
